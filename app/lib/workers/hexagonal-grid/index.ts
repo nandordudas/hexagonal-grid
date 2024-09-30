7d5dbd914584b8e2d0a101481a9c88d503ef6f6d
Nándor Dudás <nandor.dudas@gmail.com>
2024-09-30 18:28:52 +0200

    docs(readme): remove insufficient comment

--- app/lib/workers/hexagonal-grid/index.ts
+++ app/lib/workers/hexagonal-grid/index.ts
@@ -3,11 +3,6 @@ import { messageEventHandler } from './event-handlers/message.event-handler'
 
 const addEventListener = globalThis.addEventListener.bind(globalThis)
 
-/**
- * Extracting event handlers can significantly improve the performance and
- * responsiveness of your application when using hot reload, as it reduces the
- * amount of code that needs to be recompiled and re-rendered.
- */
 addEventListener('error', errorEventHandler)
 addEventListener('message', messageEventHandler)
 
